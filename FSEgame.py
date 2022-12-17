9c78673acddc5e82cd7b0e45dc1c1237ba8aa978
TheCURSEDGuy <76708528+TheCURSEDGuy@users.noreply.github.com>
2022-12-16 20:18:13 -0500

    first commit

---
+++ FSEgame.py
@@ -0,0 +1,19 @@
+# you can't figure it out :innocent:
+# skill solution where?
+# I really cnatn!!
+# Nowhere.
+print("You're mother")
+# Damn.\
+
+
+# tfym "damn" i was afk ( i was js debugging but didnt workrkkrkrrrktlkhnjkgjnkf)
+
+# did you figure it out yayyayyaya ? 
+
+#HELP 
+
+print(1)
+print(2)
+print(3)
+
+# I CANT DEBUGH 
